e35f6f772a896e7bed3fc2601ebc5e65680d9269
sanketdso <168394253+sanketdso@users.noreply.github.com>
2024-11-25 17:37:55 +0530

    Create run-buildspec.sh

---
+++ run-buildspec.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+buildspec_file="./buildspec.yml"
+if [ -f "$buildspec_file" ]
+then
+echo "Executing centralized buildspec..."
+source <(grep 'commands' "$buildspec_file")
+else
+echo "Centralized buildspec not found!"
+exit 1
+fi
